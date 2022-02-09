47524f988df38ac9f8f706bfdf432e0e98bd9196
GaneshPatil4457 <gp4457@zebra.com>
2022-02-09 19:34:19 +0530

    Initial Commit

---
+++ EmojiGame/EmojiGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiGameApp.swift
+//  EmojiGame
+//
+//  Created by Patil, Ganesh on 09/02/22.
+//
+
+import SwiftUI
+
+@main
+struct EmojiGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
